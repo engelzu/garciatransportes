d62a04ec489a14cbdb0c8ef8817d0ef164a4b1b1
engelzu <engelmobile2020@gmail.com>
2025-10-25 22:25:35 -0300

    Add files via upload

---
+++ motorista-sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('/'));
+});
